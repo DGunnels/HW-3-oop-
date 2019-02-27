6a93b44b5319f982210e47b1fc6352af1ed126c9
DGunnels <45681663+DGunnels@users.noreply.github.com>
2019-02-26 19:48:42 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,25 @@ int main() {
     grid userGridA;
     grid userGridB;
     srand(time(NULL));
+    int userX;
+    int userY;
 
+    cout << "Please input the X value (rows): " << endl;
+    cin >> userX;
+
+    cout << "Please input the Y value (columns): " << endl;
+    cin >> userY;
 
     // data input
-    userGridA.getXY();
-    userGridB.getXY();
+    userGridA.getXY(userX, userY);
+    userGridB.getXY(userX, userY);
     // printing and assigning
     userGridA.gridFormation(userGridA.getX(),userGridA.getY());
     userGridB.gridFormation(userGridB.getX(),userGridB.getY());
 
-
+    userGridA.compareGrids(userGridA.getArray(),userGridB.getArray());
+    userGridA.deleteGrids();
+    userGridB.deleteGrids();
 
 
     return 0;
