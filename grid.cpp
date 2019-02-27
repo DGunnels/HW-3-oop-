9922c735744126b1abfb300d61efcd187a8fac96
DGunnels <45681663+DGunnels@users.noreply.github.com>
2019-02-26 19:47:37 -0700

    Update grid.cpp

--- grid.cpp
+++ grid.cpp
@@ -6,19 +6,23 @@ grid::grid() {
 }
 
 int grid::getX() const {
+    // return X value
     return this->userX;
 }
 
 int grid::getY() const {
+    // return Y value
     return this->userY;
 
 }
 
 void grid::setX(int userInX) {
+    // set X value
     this->userX = userInX;
 }
 
 void grid::setY(int userInY) {
+    // set Y value
     this->userY = userInY;
 }
 
@@ -39,25 +43,22 @@ void grid::gridFormation(int getX, int getY) {
     */
 
     //cout << "Assigning Array 0's" << endl;
-    for (int i = 0; i < sizeY; i++){
-        for (int j = 0; j < sizeX; j++){
+    for (int i = 0; i < sizeX; i++){
+        for (int j = 0; j < sizeY; j++){
             gridArray[i][j] = 0;
         }
-        cout << endl;
     }
-
-
+    // assign 1's to the grid
     for (int indexVal = 0; indexVal < (sizeX*sizeY)/3; indexVal++){
         int indexPoint;
         do{
             indexPoint = rand()%(sizeX*sizeY);
-        }while (gridArray[indexPoint/sizeX][indexPoint%sizeX]==1);
-        gridArray[indexPoint/sizeX][indexPoint%sizeX]=1;
+        }while (gridArray[indexPoint/sizeY][indexPoint%sizeY]==1);
+        gridArray[indexPoint/sizeY][indexPoint%sizeY]=1;
     }
-
-    cout << "Array: " << endl;
-    for (int i = 0; i < sizeY; i++){
-        for (int j = 0; j < sizeX; j++){
+    // print the Grid
+    for (int i = 0; i < sizeX; i++){
+        for (int j = 0; j < sizeY; j++){
             cout << gridArray[i][j];
         }
         cout << endl;
@@ -65,30 +66,59 @@ void grid::gridFormation(int getX, int getY) {
 
 }
 
-void grid::getXY() {
-    int userInX;
-    int userInY;
-    cout << "Input x: " << endl;
-    cin >> userInX;
-    setX(userInX);
-    cout << "Input y: " << endl;
-    cin >> userInY;
-    setY(userInY);
+void grid::getXY(int getX, int getY) {
+    // set X/Y values for grids
+    setX(getX);
+    setY(getY);
 
 }
 
-void grid::compareGrids(int *array, int *array2) {
+void grid::compareGrids(int **array, int **array2) {
+    // create comparison grid
+    compArray = new int*[userX];
+    for (int o = 0; o < userX; o++){
+        compArray[o] = new int[userY];
+    }
+    // compare grids, assign comparison grid
+    for (int i = 0; i < userX; i++) {
+        for(int j = 0; j < userY; j++){
+            if((array[i][j] == array2[i][j]) && (array[i][j] == 1)){
+                compArray[i][j] = 1;
+            } else {
+                compArray[i][j] = 0;
+            }
+        }
+    }
+    // print comprarison grid
+    cout << "Compared Arrays: " << endl;
+    for (int i = 0; i < userX; i++){
+        for (int j = 0; j < userY; j++){
+            cout << compArray[i][j];
+        }
+        cout << endl;
+    }
 
 }
 
 void grid::gridArrayFunc(int getX, int getY) {
     int sizeX = getX;
     int sizeY = getY;
-    gridArray = new int*[sizeY];
+    gridArray = new int*[sizeX];
 
-    for (int i = 0; i < sizeY; i++) {
-        gridArray[i] = new int[sizeX];
+    for (int i = 0; i < sizeX; i++) {
+        gridArray[i] = new int[sizeY];
     }
 }
+
+int** grid::getArray() {
+    return gridArray;
+}
+
+
+void grid::deleteGrids(){
+    delete[] gridArray;
+    delete[] compArray;
+}
+
 
 
