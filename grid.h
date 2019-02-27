37d4206e42ab579d9606dfe1fa284266d058820f
DGunnels <45681663+DGunnels@users.noreply.github.com>
2019-02-26 19:48:21 -0700

    Update grid.h

--- grid.h
+++ grid.h
@@ -1,17 +1,14 @@
-//
-// Created by joker on 2/26/2019.
-//
-
 #ifndef HOMEWORK_3_GRID_H
 #define HOMEWORK_3_GRID_H
 using namespace std;
 
 class grid {
 private:
+    typedef int* arrPtr;
     int userX;
     int userY;
-    int** gridArray;
-
+    arrPtr *gridArray;
+    arrPtr *compArray;
 
 
 public:
@@ -21,10 +18,11 @@ public:
     void setX(int userInX);
     void setY(int userInY);
     void gridFormation(int getX,int getY);
-    void getXY();
-    void compareGrids(int *array, int *array2);
+    void getXY(int getX, int getY);
+    void compareGrids(int **array, int **array2);
     void gridArrayFunc(int getX, int getY);
-    void printGrid(int *array);
+    int** getArray();
+    void deleteGrids();
 };
 
 
